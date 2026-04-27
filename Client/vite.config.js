2ac28cf05a6b703cfc4eadf97ac34f0ec406780f
Krish Kumar <Krishsinghania6115@gmail.com>
2026-04-27 11:02:20 +0530

    fix: refactor Vite configuration to load environment variables for backend URL

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,15 +1,39 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+
+
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL || "http://localhost:5000",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
